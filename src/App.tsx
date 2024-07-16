4c0b8f5482e2e5707b50773cad6c1da8dff84b5f
Alan McGraw <alanmcgraw@proton.me>
2024-07-16 11:24:49 -0600

    Add calc component and set up App header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-
 import './App.css'
+import Calculator from './Calculator'
 
 export default function App() {
   return (
-    <> 
+    <>
+    <header>
+      <h1>Age in Days Calculator</h1>
+      </header>
+      <p>Given how many years old you are this app will calculatehow how many days old you are.</p>
+      <main>
+        <Calculator />
+      </main>
     </>
   )
 };
